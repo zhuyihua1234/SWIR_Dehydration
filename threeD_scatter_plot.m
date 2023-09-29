4f39f3a1682e343a07e04285f9ae26d54c008320
Yihua Zhu <78241915+zhuyihua1234@users.noreply.github.com>
2023-09-29 10:51:07 -0700

    Added function
    Added selection for Z-axis title between Integral and Derivative

--- threeD_scatter_plot.m
+++ threeD_scatter_plot.m
@@ -6,18 +6,48 @@ file = uigetfile('*.xlsx');
 file_for_save = erase(file,".xlsx");
 tbl = readtable(file);
 
+%% Select Z-Axis
+
+% Select imaging modality
+options = {'Integral at 0.1 Point', 'Deriv. at Start of Fit'};
+
+% Display a dialog box to choose between the options
+choice = listdlg('PromptString', 'Select a variable:', ...
+                 'SelectionMode', 'single', ...
+                 'ListString', options, ...
+                 'Name', 'Variable Selection');
+
+% Depending on the choice, define the selected variable
+if choice == 1
+    z_axis_title = 'Integral at 0.1 Point'; % Replace with the actual variable name
+elseif choice == 2
+    z_axis_title = 'Deriv. at Start of Fit'; % Replace with the actual variable name
+else
+    error('No variable selected.');
+end
+
+%% Import perameters
+active_exp_fit_rate = table2array(tbl(:,1));
+active_pointone_point = table2array(tbl(:,2));
+active_integral = table2array(tbl(:,3));
+
+arrested_exp_fit_rate = table2array(tbl(:,4));
+arrested_pointone_point = table2array(tbl(:,5));
+arrested_integral = table2array(tbl(:,6));
+
+
 %%
 % Generate 3D plot
 S = 100;
-scatter3(tbl.Deriv_AtFirstPointOfExp_Fit, tbl.IntersectionRelativeTo0_8Point, tbl.AreaBetweenTNCurvesFrom0_8PointToIntersectionPoint, S,'filled',"red")
+scatter3(active_exp_fit_rate, active_pointone_point, active_integral, S,'filled',"red")
 hold on
-scatter3(tbl.Deriv_AtFirstPointOfExp_Fit_1, tbl.IntersectionRelativeTo0_8Point_1, tbl.AreaBetweenTNCurvesFrom0_8PointToIntersectionPoint_1, S,'filled',"blue")
+scatter3(arrested_exp_fit_rate, arrested_pointone_point, arrested_integral, S,'filled',"blue")
 hold off
 grid on
 legend('active', 'arrested')
-xlabel('Deriv. at Start of Fit')
-ylabel('Intersection Time')
-zlabel('Area Between Curves')
+xlabel('Exponential Fit Rate')
+ylabel('0.1 Point')
+zlabel(z_axis_title)
 savefig(file_for_save)
 
 %%
